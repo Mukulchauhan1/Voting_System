f1ecc6b3b40bfdac148f9e00629abc442e2f6ad2
Mukulchauhan1 <97191692+Mukulchauhan1@users.noreply.github.com>
2023-07-08 16:43:54 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,82 @@
+// ------------------------------------ Project Name : Voting System -----------------------------------------
+
+// --------------------------------- Projected Created on 24/06/2023 ------------------------
+
+#include<bits/stdc++.h>
+#include "voter.h"
+#include "database.h"
+#include "candidate.h"
+#include "welcome.h"
+#include<stdlib.h>
+#include<fstream>
+
+using namespace std;
+int main(){
+    // Variable Declaration
+    ifstream fileIn;
+    int nCand, nVoter;
+    Welcome k;
+    k.voting();
+    cout<<endl<<endl<<"Press key to enter : ";
+    cin.ignore();
+    system("CLS");
+    
+    // Step 1 : Creating the Database for candidate and voter
+    DBC DbCandidate;
+    DBV DbVoter;
+
+    // Step 2 : Taking cruical detail 
+    fileIn.open("inputData.txt");
+    fileIn>>nCand>>nVoter;
+
+    // Step 3 : Creating Candidate profile
+    for(int i=0;i<nCand;i++){
+        cout<<"--------------------------------Fill the Candidate Detail--------------------------------"<<endl;
+        // Creating a candidate
+        Candidate *cand = new Candidate();
+        cand->AddDetail();
+        // Adding Candidate to the candidate database
+        DbCandidate.Add(cand);
+        system("CLS");
+    }
+
+    
+    for(int i=0;i<nVoter;i++){
+        cout<<"--------------------------------Fill the voter Details--------------------------------"<<endl;
+        // Creating the voter
+        Voter* vt = new Voter();
+        pair<string,string> VV;
+        VV.first = vt->AddDetail();
+        // Adding Voter to voter database
+        VV.second = DbVoter.Add(vt);
+
+        // Starting voting
+        cout<<endl<<"--------------------------------Start Voting--------------------------------"<<endl<<endl;
+        cout<<"Candidate No : Candidate Name -> Candidate ID"<<endl<<endl;
+        DbCandidate.ListC();
+        cout<<endl<<endl<<"------------------------------------------------------------"<<endl<<endl;
+        // Checking Voter ID of voter
+        string id;
+        cout<<"Enter your Voter ID : ";
+        cin>>id;
+
+        // Checking if there voter exist with this ID and also is he eligible for vote
+        if(DbVoter.check(id)){
+            DbCandidate.GiveVote();
+            DbVoter.VoteMark(VV.second);
+        }
+        else{
+            cout<<endl<<"--------------------------------Voter is Not Eligible to Vote--------------------------------"<<endl;
+            cin.ignore();
+        }
+
+        cin.ignore();
+        system("CLS");
+    }
+
+    cout<<endl<<"--------------------------------Voting Result--------------------------------"<<endl<<endl;
+    // Step 8 : Declaration of Results
+    DbCandidate.DeclareResult();
+
+    return 0;
+}
